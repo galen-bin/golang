ea994d6a56930601c926e9ab5ea16e7a941926b4
Del Greaf <234273537@qq.com>
2025-09-17 17:31:11 +0800

    golang 调用合约接口

---
+++ 201/index.go
@@ -0,0 +1,528 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math"
+	"math/big" // for demo
+	"os"
+	"strings"
+	"time"
+
+	token "201/erc20" // for demo
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"golang.org/x/crypto/sha3"
+)
+
+const adres = "https://ethereum-sepolia-rpc.publicnode.com"
+
+func main() {
+	//余额查询
+	//search_blance();
+	//区信息
+	//serch_BlockReceipts();
+	//创建账号
+	//create_key()
+	//etc转账
+	//tranc()
+	//defi转账
+	//defi()
+	//etc_blance()
+	//defi_blance()
+	api_etch()
+
+}
+
+func search_block() {
+
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(8988823)
+
+	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	fmt.Println(header.Number.Uint64())     // 5671744
+	fmt.Println(header.Time)                // 1712798400
+	fmt.Println(header.Difficulty.Uint64()) // 0
+	fmt.Println(header.Hash().Hex())        // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(block.Number().Uint64())     // 5671744
+	fmt.Println(block.Time())                // 1712798400
+	fmt.Println(block.Difficulty().Uint64()) // 0
+	fmt.Println(block.Hash().Hex())          // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+	fmt.Println(len(block.Transactions()))   // 70
+	count, err := client.TransactionCount(context.Background(), block.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(count) // 70
+
+}
+
+func search_blance() {
+
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chainID, err := client.ChainID(context.Background())
+	fmt.Println(types.NewEIP155Signer(chainID))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(8988823)
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, tx := range block.Transactions() {
+		fmt.Println(tx.Hash().Hex())        // 0xc507884fcb02f904348ccdd289c8687207668f2d20ed3a183cd7a7adc5af5d74
+		fmt.Println(tx.Value().String())    // 52429830000000000
+		fmt.Println(tx.Gas())               // 30000
+		fmt.Println(tx.GasPrice().Uint64()) // 500000000000
+		fmt.Println(tx.Nonce())             // 3850854
+		fmt.Println(tx.Data())              // []
+		fmt.Println(tx.To().Hex())
+		// 0x749458ac875a1De3C446645fD98fBC00E8E11B4d
+
+		fmt.Println(chainID)
+
+		if sender, err := types.Sender(types.NewPragueSigner(chainID), tx); err == nil {
+			fmt.Println("sender", sender.Hex()) // 0x6Cc9397c3B38739daCbfaA68EaD5F5D77Ba5F455
+		} else {
+			log.Fatal(err)
+		}
+
+		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(receipt.Status) // 1
+		fmt.Println(receipt.Logs)   // []
+		break
+	}
+
+	blockHash := common.HexToHash("*")
+
+	count, err := client.TransactionCount(context.Background(), blockHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for idx := uint(0); idx < count; idx++ {
+		tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(tx.Hash().Hex()) // 0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5
+		break
+	}
+
+	txHash := common.HexToHash("*")
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(isPending)
+	fmt.Println(tx.Hash().Hex()) // 0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5.Println(isPending)
+
+}
+
+func serch_BlockReceipts() {
+
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(5671744)
+	blockHash := common.HexToHash("*")
+
+	receiptByHash, err := client.BlockReceipts(context.Background(), rpc.BlockNumberOrHashWithHash(blockHash, false))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	receiptsByNum, err := client.BlockReceipts(context.Background(), rpc.BlockNumberOrHashWithNumber(rpc.BlockNumber(blockNumber.Int64())))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(receiptByHash[0] == receiptsByNum[0]) // true
+
+	for _, receipt := range receiptByHash {
+		fmt.Println(receipt.Status)                // 1
+		fmt.Println(receipt.Logs)                  // []
+		fmt.Println(receipt.TxHash.Hex())          // 0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5
+		fmt.Println(receipt.TransactionIndex)      // 0
+		fmt.Println(receipt.ContractAddress.Hex()) // 0x0000000000000000000000000000000000000000
+		break
+	}
+
+	txHash := common.HexToHash("*")
+	receipt, err := client.TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(receipt.Status)                // 1
+	fmt.Println(receipt.Logs)                  // []
+	fmt.Println(receipt.TxHash.Hex())          // 0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5
+	fmt.Println(receipt.TransactionIndex)      // 0
+	fmt.Println(receipt.ContractAddress.Hex()) // 0x0000000000000000000000000000000000000000
+}
+
+func create_key() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes)[2:]) // 去掉'0x'
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println("from pubKey:", hexutil.Encode(publicKeyBytes)[4:]) // 去掉'0x04'
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address)
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(publicKeyBytes[1:])
+	fmt.Println("full:", hexutil.Encode(hash.Sum(nil)[:]))
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:])) // 原长32位，截去12位，保留后20位
+}
+
+func tranc() {
+
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//a1 d6ba9dd24309ef43a7bc4c4ae2299414feb125a6eab08b21370c6c45cfe39460
+
+	privateKey, err := crypto.HexToECDSA("*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(800000000000000000) // in wei (1 eth)
+	gasLimit := uint64(21000)               // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress("*")
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+
+}
+
+func defi() {
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA("*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(0) // in wei (0 eth)
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress(fromAddress.Hex())
+	tokenAddress := common.HexToAddress("0xa1c19ff3ae0B85AB6B7EC8A930e276a5f663D400")
+
+	transferFnSignature := []byte("transfer(address,uint256)")
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(transferFnSignature)
+	methodID := hash.Sum(nil)[:4]
+	fmt.Println(hexutil.Encode(methodID)) // 0xa9059cbb
+	paddedAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
+	fmt.Println(hexutil.Encode(paddedAddress)) // 0x0000000000000000000000004592d8f8d7b001e72cb26a73e4fa1806a51ac79d
+	amount := new(big.Int)
+	amount.SetString("1000000000000000000000", 10) // 1000 tokens
+	paddedAmount := common.LeftPadBytes(amount.Bytes(), 32)
+	fmt.Println(hexutil.Encode(paddedAmount)) // 0x00000000000000000000000000000000000000000000003635c9adc5dea00000
+	var data []byte
+	data = append(data, methodID...)
+	data = append(data, paddedAddress...)
+	data = append(data, paddedAmount...)
+
+	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+		To:   &toAddress,
+		Data: data,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(gasLimit) // 23256
+	tx := types.NewTransaction(nonce, tokenAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex()) // tx sent: 0xa56316b637a94c4cc0331c73ef26389d6c097506d581073f927275e7a6ece0bc
+}
+
+func etc_blance() {
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("*")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+	blockNumber := big.NewInt(9165433)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 25729324269165216042
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
+}
+
+func defi_blance() {
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Golem (GNT) Address
+	tokenAddress := common.HexToAddress("0x40e316F3E5C4E6B3853A1284C534C8E4E22229eb")
+	instance, err := token.NewGGToken(tokenAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	address := common.HexToAddress("*")
+	bal, err := instance.BalanceOf(&bind.CallOpts{}, address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	name, err := instance.Name(&bind.CallOpts{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	symbol, err := instance.Symbol(&bind.CallOpts{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	decimals, err := instance.Decimals(&bind.CallOpts{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("name: %s\n", name)         // "name: Golem Network"
+	fmt.Printf("symbol: %s\n", symbol)     // "symbol: GNT"
+	fmt.Printf("decimals: %v\n", decimals) // "decimals: 18"
+	fmt.Printf("wei: %s\n", bal)           // "wei: 74605500647408739782407023"
+	fbal := new(big.Float)
+	fbal.SetString(bal.String())
+	value := new(big.Float).Quo(fbal, big.NewFloat(math.Pow10(int(decimals))))
+	fmt.Printf("balance: %f", value) // "balance: 74605500.647409"
+}
+
+func api_etch() {
+
+	client, err := ethclient.Dial(adres)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contractAddr := "0x6Fa7438fd5Ca09e1556555e035E31fe06215A152"
+	// 准备交易数据
+	json_api, err := os.ReadFile("./api.json")
+	if err != nil {
+		fmt.Println("读取文件失败:", err)
+		return
+	}
+
+	contractABI, err := abi.JSON(strings.NewReader(string(json_api)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	privateKey, err := crypto.HexToECDSA("*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 获取公钥地址
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	// 获取 nonce
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 估算 gas 价格
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	methodName := "setitem"
+	input, err := contractABI.Pack(methodName, "key01", big.NewInt(2000))
+
+	// 创建交易并签名交易
+	chainID, err := client.NetworkID(context.Background())
+	tx := types.NewTransaction(nonce, common.HexToAddress(contractAddr), big.NewInt(0), 300000, big.NewInt(0).Add(gasPrice, big.NewInt(10000)), input)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 发送交易
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Transaction sent: %s\n", signedTx.Hash().Hex())
+	_, err = waitForReceipt(client, signedTx.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查询刚刚设置的值
+	callInput, err := contractABI.Pack("item", common.HexToAddress("*"), "key01")
+	//callInput, err := contractABI.Pack("getitem", "key01")
+	if err != nil {
+		log.Fatal(err)
+	}
+	to := common.HexToAddress(contractAddr)
+	callMsg := ethereum.CallMsg{
+		From: fromAddress,
+		To:   &to,
+		Data: callInput,
+	}
+
+	// 解析返回值
+	result, err := client.CallContract(context.Background(), callMsg, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//	var unpacked int
+	//contractABI.UnpackIntoInterface(&unpacked, "item", result)
+	unpacked, _ := contractABI.Unpack("item", result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("is value saving in contract equals to origin value:", unpacked)
+
+}
+
+func waitForReceipt(client *ethclient.Client, txHash common.Hash) (*types.Receipt, error) {
+	for {
+		receipt, err := client.TransactionReceipt(context.Background(), txHash)
+		if err == nil {
+			return receipt, nil
+		}
+		if err != ethereum.NotFound {
+			return nil, err
+		}
+		// 等待一段时间后再次查询
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ 201/mytoken.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.8;
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    mapping (address => mapping (string => uint256)) public item; 
+    
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() public{
+        name = "MyToken"; 
+        symbol = "MTK"; 
+        decimals = 18; 
+        totalSupply = 100000000 * 10 ** uint256(decimals);
+
+        balances[msg.sender] = totalSupply;  
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];    
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= _value;    
+        balances[_to] += _value;   
+
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balances[_from] >= _value, "ERC20: transfer amount exceeds balance");
+        require(allowances[_from][msg.sender] >= _value,"ERC20: transfer amount exceeds allowance");
+
+        balances[_from] -= _value; 
+        balances[_to] += _value; 
+
+        allowances[_from][msg.sender] -= _value;
+        
+        emit Transfer(_from, _to, _value); 
+        return true; 
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowances[msg.sender][_spender] = _value; 
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowances[_owner][_spender];
+    }
+
+    function setitem(string calldata key,uint256 val) external  {
+    
+            item[msg.sender][key]=val;
+     
+    }
+
+    function getitem(string memory key)public view returns(uint256){
+        return item[msg.sender][key];
+    }
+  
+    
+}
